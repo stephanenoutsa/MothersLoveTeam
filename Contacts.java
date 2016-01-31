e17eb806c9c95ee40cb90e426284f47b9d5ca5a5
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-01-31 14:00:19 +0100

    Retrieves all contacts from database and prints them on contacts activity

---
+++ Contacts.java
@@ -0,0 +1,40 @@
+package com.motherslove.stephnoutsa.mothersloveteam;
+
+import android.os.Bundle;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class Contacts extends AppCompatActivity {
+
+    TextView contacts;
+    String dbString;
+    MyDBHandler dbHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+        contacts = (TextView) findViewById(R.id.contacts);
+        dbHandler = new MyDBHandler(this, null, null, 1);
+        dbString = dbHandler.databaseToString();
+        contacts.setText(dbString);
+    }
+
+}
