dcfd012d2f1f499f3a36e56c5c5cdc349e78f691
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-02-05 10:21:23 +0100

    Adds more functions to perform all CRUD operations

--- MyDBHandler.java
+++ MyDBHandler.java
@@ -7,6 +7,9 @@ import android.database.Cursor;
 import android.content.Context;
 import android.content.ContentValues;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyDBHandler extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
@@ -44,6 +47,67 @@ public class MyDBHandler extends SQLiteOpenHelper {
         SQLiteDatabase db = getWritableDatabase();
         db.insert(TABLE_CONTACTS, null, values);
         db.close();
+    }
+
+    // Get single contact from the CONTACTS table
+    public Contact getContact(int id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Cursor c = db.query(TABLE_CONTACTS, new String[] {CONTACT_COLUMN_ID, CONTACT_COLUMN_PHONE,
+                CONTACT_COLUMN_LMP}, CONTACT_COLUMN_ID + "=?", new String[] {String.valueOf(id)},
+                null, null, null, null);
+        if(c != null)
+            c.moveToFirst();
+
+        Contact contact = new Contact(Integer.parseInt(c.getString(0)), c.getString(1), c.getString(2));
+
+        return contact;
+    }
+
+    // Get all contacts from the CONTACTS table
+    public List<Contact> getAllContacts() {
+        List<Contact> contactList = new ArrayList<Contact>();
+
+        String query = "SELECT * FROM " + TABLE_CONTACTS;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor c = db.rawQuery(query, null);
+
+        // Loop through all rows and add to list
+        if(c.moveToFirst()) {
+            do {
+                Contact contact = new Contact();
+                contact.set_contactid(Integer.parseInt(c.getString(0)));
+                contact.setContactphone(c.getString(1));
+                contact.setContactlmp(c.getString(2));
+
+                contactList.add(contact);
+            } while (c.moveToNext());
+        }
+
+        return contactList;
+    }
+
+    // Get contacts count
+    public int getContactsCount() {
+        String query = "SELECT * FROM " + TABLE_CONTACTS;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery(query, null);
+        c.close();
+
+        return c.getCount();
+    }
+
+    // Update single contact in the CONTACTS table
+    public int updateContact(Contact contact) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(CONTACT_COLUMN_PHONE, contact.getContactphone());
+        values.put(CONTACT_COLUMN_LMP, contact.getContactlmp());
+
+        return db.update(TABLE_CONTACTS, values, CONTACT_COLUMN_ID + "=?",
+                new String[] {String.valueOf(contact.get_contactid())});
     }
 
     // Delete a contact from the CONTACTS table
